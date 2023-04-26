748858a8f87ef18dd84761f53a8b271e46801acc
sarmpoy <118840155+sarmpoy@users.noreply.github.com>
2023-04-26 18:24:28 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
 basic.forever(function () {
-	
+    if (cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters) < 10) {
+        cuteBot.moveTime(cuteBot.Direction.backward, 25, 1)
+        cuteBot.moveTime(cuteBot.Direction.right, 50, 1)
+    } else {
+        cuteBot.motors(25, 25)
+    }
 })
